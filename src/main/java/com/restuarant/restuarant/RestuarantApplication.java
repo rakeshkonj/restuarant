f8db4807605c605f27cf555bce11372ac9bf250d
Rakesh_k <ksingh@healthedge.com>
2025-01-23 14:58:57 +0530

    package structure

--- src/main/java/com/restuarant/restuarant/RestuarantApplication.java
+++ src/main/java/com/restuarant/restuarant/RestuarantApplication.java
@@ -2,7 +2,9 @@ package com.restuarant.restuarant;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
+@ComponentScan(basePackages = {"com.restuarant.*"})
 @SpringBootApplication
 public class RestuarantApplication {
 
